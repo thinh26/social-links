d292302781a2322143c33a5dd84b7b59f8deb5d0
Nguyen Duc Thinh <ducthinh4420@gmail.com>
2025-07-21 22:30:30 +0700

    enhance bundling

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-let CACHE_DYNAMIC_NAME = "dynamic-v1";
+let CACHE_DYNAMIC_NAME = "dynamic-v2";
 
 self.addEventListener("install", function (event) {
   console.log("[Service Worker] Installing Service Worker ....", event);
